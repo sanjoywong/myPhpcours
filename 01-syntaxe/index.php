ce75049f949e82ed7f56479453cc93db59f448d4
sanjoy.wong <sanjoy.wong@gmail.com>
2022-05-09 11:31:30 +0200

    declaration de variables

--- 01-syntaxe/index.php
+++ 01-syntaxe/index.php
@@ -3,12 +3,61 @@
 <?php
 // echo/print
 echo ' Texte en php' , ' Autre texte<br>';
-
-print 'Texte en php avec print<br>' , 'Autre texte avec print<br>';
+// print peut seulment avoir une parametre
+print 'Texte en php avec print<br>' ;
+//, 'Autre texte avec print<br>';
 // un commentaire
 # un commentaire
 
-/* commentaires */
+/* commentaires 
+sur plusieurs
+lignes
+*/
+
+//variables
+ $variable = 'Une variale';
+ $variable = 'Une autre variable';
+ echo $variable,'----------------',$variable,'<br>';
+// c'est pas bon : $3var = 34;
+$_var = 34;
+
+echo $_var;
+
+$camlCase = 'une variable en camel';
+$snake_case = 'une variable en snake case';
+$PascaleCase = 'une variable pascale case';
+$var = 'une variable en kebab-case';
+
+// string
+$nom = 'mohamed';
+// int
+$nombre = 34;
+// float
+$nombreDecimale = 23.7;
+// boolean
+$vrai = true;
+$faux = false;
+// array/objet
+$tableau =['grouge',34,45.6,false];
+// null
+$variable = null;
+echo '<br>';
+// simple/double guillemets (quotes)
+
+//$message = "aujourd'hui";
+$message = 'ah ah; aujourd\'hui';
+echo $message;
+$txt = 'Bonjour';
+echo '<br>','$txt tout le monde <br>';
+echo "$txt tout le monde";
+
+$fruit1 = 'Kiwi';
+$fruit1 = 'Pomme';
+$fruit2 = 'Banane';
+
+echo '<br>',$fruit1,'<br>'.$fruit2;
+
+
 
 
 ?>
