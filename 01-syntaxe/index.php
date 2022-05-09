daec576d4ce75f6e20c21cc3eb868eb4796527a0
sanjoy.wong <sanjoy.wong@gmail.com>
2022-05-09 10:30:37 +0200

    commentaire en php

---
+++ 01-syntaxe/index.php
@@ -0,0 +1,14 @@
+<h1> Un titre</h1>
+
+<?php
+// echo/print
+echo ' Texte en php' , ' Autre texte<br>';
+
+print 'Texte en php avec print<br>' , 'Autre texte avec print<br>';
+// un commentaire
+# un commentaire
+
+/* commentaires */
+
+
+?>
