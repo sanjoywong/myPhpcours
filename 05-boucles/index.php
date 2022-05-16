f1da1f618e96a0c753a565bb8fa57f6d54e89ba3
sanjoy.wong <sanjoy.wong@gmail.com>
2022-05-16 15:25:59 +0200

    'calcule l'année

--- 05-boucles/index.php
+++ 05-boucles/index.php
@@ -9,4 +9,18 @@ while($j <=10){
     $j++;
 }
 
+//  Avec une boucle while afficher toutes les années de 1970 à aujoud'hui dans une liste ul
+// Affichier le nombre de jour depus 1970 
+// travailler dans une branche annees
+ $i = 1970;
+ echo "<ul>";
+ while($i <= date('Y')){
+     echo "<li> $i </li>";
+     $i++;
+ };
+ echo "</ul>";
+
+ var_dump(date("Y-m-d"));
+ $cle =strtotime(date("Y-m-d"))-strtotime( "1975.01.01");
+echo floor($cle/3600/24);
 ?>
